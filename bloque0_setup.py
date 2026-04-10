03e1dc7bf4d06a44f90edda2b37790c7c77f850c
Ivan Guachamin <ivanandresguachamin1506@gmail.com>
2026-04-09 23:08:38 -0500

    Descarga e import de bibliotecas

---
+++ bloque0_setup.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+print('pandas version:', pd.__version__)
+
+print("matplotlib importado correctamente", plt.__version__)
+
+print('Todo listo para el taller')
